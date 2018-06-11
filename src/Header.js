e73ac99ac5beb35fd26f271fdd5ad99356f8f799
FilipSantic <filip.santic.95@gmail.com>
2018-06-11 03:11:49 +0200

    Treći commit

--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './Header.css';
 
 const API = 'http://www.colr.org/json/color/random';
 
@@ -9,7 +10,13 @@ class Header extends Component {
       this.state = {
         colors1: [],
         colors2: [],
+        isToggleOn: true,
+        value: '',
       };
+
+      this.handleClick = this.handleClick.bind(this);
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
     }
   
     componentDidMount() {
@@ -23,26 +30,54 @@ class Header extends Component {
             colors2: data2.colors
         }));
     }
+
+    handleClick() {
+      this.setState(prevState => ({
+        isToggleOn: !prevState.isToggleOn
+      }));
+    }
+
+    handleChange(event) {
+      this.setState({value: event.target.value});
+    }
+  
+    handleSubmit(event) {
+      event.preventDefault();
+    }
   
     render() {
-        var colors1 = this.state.colors1;
-        var colors2 = this.state.colors2;
+        const { colors1 } = this.state;
+        const { colors2 } = this.state;
+
+        const colors = [
+          colors1.map(color1 => color1.hex),
+          colors2.map(color2 => color2.hex)
+        ]
+
+        const randomColor = '#' + colors[Math.floor(Math.random()*colors.length)];
+        const blackColor = '#000000';
     
         return (
           <div class="Header">
-            {colors1.map(color1 =>
-              <div key={color1.id}>
-                {color1.hex}
-              </div>
-            )}
-            {colors2.map(color2 =>
-              <div key={color2.id}>
-                {color2.hex}
-              </div>
-            )}
+            <div>
+              Click on the button below to change it's color.
+            </div>
+            <div>
+            <button className="Button" style={{backgroundColor: this.state.isToggleOn ? randomColor : blackColor}} onClick={this.handleClick}>
+            {this.state.value}
+            </button>
+            </div>
+            <div>
+              <form onSubmit={this.handleSubmit}>
+                <label className="Form">
+                  Change the button text:
+                  <input className="Form" type="text" value={this.state.value} onChange={this.handleChange}/>
+                </label>
+              </form>
+            </div>
           </div>
         );
     }
-  }
+}
 
 export default Header;
